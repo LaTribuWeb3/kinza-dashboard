b988df1e5ed0b004ec62a64d5e8c45d458b97353
Alfu <Alfulinku@gmail.com>
2023-09-26 13:40:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Box, Link, Typography } from '@mui/material';
+import './App.css';
+
+function App() {
+
+  return (
+    <Box>
+      
+      <Typography variant="h1">La Tribu Web3</Typography>
+      <Typography variant='h2'>Front End Boilerplate</Typography>
+      <Typography variant='h3'>Vite + React + TS</Typography>
+      <Link href="mailto:hello@la-tribu.xyz">Get in touch</Link>
+    </Box>
+  );
+}
+
+export default App;
