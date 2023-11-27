9ea855055dae45e8719f6ae6eeada7645ababdba
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-11-27 15:36:52 +0100

    fetching data

--- src/routes/risklevels/RiskLevels.tsx
+++ src/routes/risklevels/RiskLevels.tsx
@@ -1,5 +1,54 @@
 import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import DataService from "../../services/DataService";
+import { Pair } from "../../models/ApiData";
+import { sleep } from "../../utils/Utils";
+import { SimpleAlert } from "../../components/SimpleAlert";
 
 export default function RiskLevels() {
-    return (<Box>BITCONNEEEEEEEEEEECT</Box>)
+  const [isLoading, setIsLoading] = useState(true);
+  const [availablePairs, setAvailablePairs] = useState<Pair[]>([]);
+  const [selectedPair, setSelectedPair] = useState<Pair>();
+  const [openAlert, setOpenAlert] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
+
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    // Define an asynchronous function
+    async function fetchData() {
+      try {
+        const data = await DataService.GetAvailablePairs('all');
+        setAvailablePairs(data);
+
+        const oldPair = selectedPair;
+
+        if (oldPair && data.some((_) => _.base == oldPair.base && _.quote == oldPair.quote)) {
+          setSelectedPair(oldPair);
+        } else {
+          setSelectedPair(data[0]);
+        }
+        await sleep(1); // without this sleep, update the graph before changing the selected pair. so let it here
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAlert(true);
+        setIsLoading(false);
+        if (error instanceof Error) {
+          setAlertMsg(`Error fetching data: ${error.toString()}`);
+        } else {
+          setAlertMsg(`Unknown error`);
+        }
+      }
+    }
+    fetchData()
+      .then(() => setIsLoading(false))
+      .catch(console.error);
+  }, []);
+
+    return (<Box>BITCONNEEEEEEEEEEECT
+      <SimpleAlert alertMsg={alertMsg} handleCloseAlert={handleCloseAlert} openAlert={openAlert} />
+    </Box>)
 }
