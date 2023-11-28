277fd5a2e88b835695ffae526a11be3b64cd52a2
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-11-28 16:50:18 +0100

    prettier

--- src/routes/risklevels/RiskLevelGraph.tsx
+++ src/routes/risklevels/RiskLevelGraph.tsx
@@ -3,157 +3,165 @@ import { LiquidityData, Pair } from '../../models/ApiData';
 import DataService from '../../services/DataService';
 import { Grid, LinearProgress, Skeleton, Typography } from '@mui/material';
 import { SimpleAlert } from '../../components/SimpleAlert';
-import { sleep } from '../../utils/Utils';
+import { FriendlyFormatNumber, sleep } from '../../utils/Utils';
 import moment from 'moment';
 import { MORPHO_RISK_PARAMETERS_ARRAY } from '../../utils/Constants';
+import Graph from '../../components/Graph';
 export interface RiskLevelGraphsInterface {
-    pair: Pair;
-    platform: string;
-    supplyCap: number
+  pair: Pair;
+  platform: string;
+  supplyCap: number;
 }
-
 
 export interface GraphDataAtBlock {
-    blockNumber: number,
-    [property:string]: number
-    
+  blockNumber: number;
+  [property: string]: number;
 }
+
 function RiskLevelGraphsSkeleton() {
-    return (
-        <Grid mt={5} container spacing={0}>
-            <LinearProgress color="secondary" sx={{ position: 'absolute', bottom: 5, left: 0, width: '100vw' }} />
-            <Grid item xs={12}>
-                <Skeleton height={500} variant="rectangular" />
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid mt={5} container spacing={0}>
+      <LinearProgress color="secondary" sx={{ position: 'absolute', bottom: 5, left: 0, width: '100vw' }} />
+      <Grid item xs={12}>
+        <Skeleton height={500} variant="rectangular" />
+      </Grid>
+    </Grid>
+  );
 }
-function findRiskLevelFromParameters(volatility:number, liquidity:number, liquidationBonus:number, ltv:number, borrowCap:number) {
-    const sigma = volatility;
-    const d = borrowCap;
-    const beta = liquidationBonus;
-    const l = liquidity;
-    ltv = Number(ltv) / 100;
+function findRiskLevelFromParameters(
+  volatility: number,
+  liquidity: number,
+  liquidationBonus: number,
+  ltv: number,
+  borrowCap: number
+) {
+  const sigma = volatility;
+  const d = borrowCap;
+  const beta = liquidationBonus;
+  const l = liquidity;
+  ltv = Number(ltv) / 100;
 
-    const sigmaTimesSqrtOfD = sigma * Math.sqrt(d);
-    const ltvPlusBeta = ltv + beta;
-    const lnOneDividedByLtvPlusBeta = Math.log(1/ltvPlusBeta);
-    const lnOneDividedByLtvPlusBetaTimesSqrtOfL = lnOneDividedByLtvPlusBeta * Math.sqrt(l);
-    const r = sigmaTimesSqrtOfD / lnOneDividedByLtvPlusBetaTimesSqrtOfL;
+  const sigmaTimesSqrtOfD = sigma * Math.sqrt(d);
+  const ltvPlusBeta = ltv + beta;
+  const lnOneDividedByLtvPlusBeta = Math.log(1 / ltvPlusBeta);
+  const lnOneDividedByLtvPlusBetaTimesSqrtOfL = lnOneDividedByLtvPlusBeta * Math.sqrt(l);
+  const r = sigmaTimesSqrtOfD / lnOneDividedByLtvPlusBetaTimesSqrtOfL;
 
-    return r;
+  return r;
 }
 
 export function RiskLevelGraphs(props: RiskLevelGraphsInterface) {
-    const [liquidityData, setLiquidityData] = useState<LiquidityData>();
-    const [isLoading, setIsLoading] = useState(true);
-    const [openAlert, setOpenAlert] = useState(false);
-    const [alertMsg, setAlertMsg] = useState('');
+  const [liquidityData, setLiquidityData] = useState<LiquidityData>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [openAlert, setOpenAlert] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
+  const [graphData, setGraphData] = useState<GraphDataAtBlock[]>([]);
 
-    const handleCloseAlert = () => {
-        setOpenAlert(false);
-    };
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
 
-    useEffect(() => {
-        setIsLoading(true);
-        async function fetchAndComputeDataForGraph() {
-            try {
-                const data = await DataService.GetLiquidityData(props.platform, props.pair.base, props.pair.quote);
-                const graphData:GraphDataAtBlock[] = [];
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchAndComputeDataForGraph() {
+      try {
+        const data = await DataService.GetLiquidityData(props.platform, props.pair.base, props.pair.quote);
+        const graphData: GraphDataAtBlock[] = [];
 
+        /// get token price
+        const liquidityObjectToArray = Object.keys(data.liquidity).map((_) => parseInt(_));
+        const maxBlock = Math.max.apply(null, liquidityObjectToArray).toString();
+        const tokenPrice = data.liquidity[maxBlock].priceMedian;
 
-                /// get token price
-                const liquidityObjectToArray = (Object.keys(data.liquidity)).map((_)=> parseInt(_));
-                const maxBlock = (Math.max.apply(null, liquidityObjectToArray)).toString();
-                const tokenPrice = data.liquidity[maxBlock].priceMedian;
-
-                /// for each block
-                for(const [block, blockData] of Object.entries(data.liquidity)){
-                    const currentBlockData:GraphDataAtBlock = {
-                        blockNumber: Number(block)
-                    }
-                    MORPHO_RISK_PARAMETERS_ARRAY.forEach(
-                        (_) => {
-                            const liquidationBonus = _.bonus;
-                            const liquidity = blockData.slippageMap[liquidationBonus].base * tokenPrice;
-                            const ltv = _.ltv;
-                            const borrowCap = props.supplyCap * tokenPrice;
-                            currentBlockData[`${_.bonus}_${_.ltv}`] = findRiskLevelFromParameters(blockData.volatility, liquidity, liquidationBonus, ltv, borrowCap);
-                        }
-                    )
-                    graphData.push(currentBlockData);
-                }
-
-                console.log('graphData', graphData);
-                setLiquidityData(data);
-                await sleep(1);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                setOpenAlert(true);
-                setIsLoading(false);
-                if (error instanceof Error) {
-                    setAlertMsg(`${error.toString()}`);
-                } else {
-                    setAlertMsg(`Unknown error`);
-                }
-            }
+        /// for each block
+        for (const [block, blockData] of Object.entries(data.liquidity)) {
+          const currentBlockData: GraphDataAtBlock = {
+            blockNumber: Number(block)
+          };
+          MORPHO_RISK_PARAMETERS_ARRAY.forEach((_) => {
+            const liquidationBonus = _.bonus;
+            const liquidity = blockData.slippageMap[liquidationBonus].base * tokenPrice;
+            const ltv = _.ltv;
+            const borrowCap = props.supplyCap * tokenPrice;
+            currentBlockData[`${_.bonus}_${_.ltv}`] = findRiskLevelFromParameters(
+              blockData.volatility,
+              liquidity,
+              liquidationBonus,
+              ltv,
+              borrowCap
+            );
+          });
+          graphData.push(currentBlockData);
         }
-        fetchAndComputeDataForGraph()
-            .then(() => setIsLoading(false))
-            .catch(console.error);
-            console.log(liquidityData);
-        // platform is not in the deps for this hooks because we only need to reload the data
-        // if the pair is changing
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.pair.base, props.pair.quote, props.supplyCap]);
-
-    if (!liquidityData) {
-        return <RiskLevelGraphsSkeleton />;
+        graphData.sort((a, b) => a.blockNumber - b.blockNumber);
+        setGraphData(graphData);
+        setLiquidityData(data);
+        await sleep(1);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAlert(true);
+        setIsLoading(false);
+        if (error instanceof Error) {
+          setAlertMsg(`${error.toString()}`);
+        } else {
+          setAlertMsg(`Unknown error`);
+        }
+      }
     }
+    fetchAndComputeDataForGraph()
+      .then(() => setIsLoading(false))
+      .catch(console.error);
+    console.log(liquidityData);
+    // platform is not in the deps for this hooks because we only need to reload the data
+    // if the pair is changing
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.pair.base, props.pair.quote, props.supplyCap]);
 
-    const updated = moment(liquidityData.updated).fromNow();
+  if (!liquidityData) {
+    return <RiskLevelGraphsSkeleton />;
+  }
 
-    return (
-        <>
-            {isLoading ? (
-                <RiskLevelGraphsSkeleton />
-            ) : (
-                <Grid width={'100vw'} container spacing={0}>
-                    <Grid item xs={12}>
-                        <Typography
-                            textAlign={'center'}
-                            mt={2}
-                        >{`${props.pair.base}/${props.pair.quote} data over 180 days (updated ${updated})`}</Typography>
-                    </Grid>
+  const updated = moment(liquidityData.updated).fromNow();
+  return (
+    <>
+      {isLoading ? (
+        <RiskLevelGraphsSkeleton />
+      ) : (
+        <Grid width={'100vw'} container spacing={0}>
+          <Grid item xs={12}>
+            <Typography
+              textAlign={'center'}
+              mt={2}
+            >{`${props.pair.base}/${props.pair.quote} data over 180 days (updated ${updated})`}</Typography>
+          </Grid>
 
-                    {/* probably map output to graph */}
-                    <Grid item xs={12} lg={6}>
-                        {/* <Graph
-                            title={`${props.pair.base}/${props.pair.quote} liquidity`}
-                            xAxisData={Object.keys(liquidityData.liquidity).map((_) => Number(_))}
-                            xAxisLabel="Block"
-                            leftYAxis={{ min: 0, formatter: FriendlyFormatNumber }}
-                            rightYAxis={{ min: 0, formatter: FriendlyFormatNumber }}
-                            leftAxisSeries={[
-                                {
-                                    label: `Amount ${props.pair.base} sold`,
-                                    data: Object.values(liquidityData.liquidity).map((_) => _.slippageMap[props.targetSlippage].base),
-                                    formatter: FriendlyFormatNumber
-                                }
-                            ]}
-                            rightAxisSeries={[
-                                {
-                                    label: `Amount ${props.pair.quote} received`,
-                                    data: Object.values(liquidityData.liquidity).map((_) => _.slippageMap[props.targetSlippage].quote),
-                                    formatter: FriendlyFormatNumber
-                                }
-                            ]}
-                        /> */}
-                    </Grid>
-                </Grid>
-            )}
+          <Grid item xs={12} lg={6}>
+            <Graph
+              title={`${props.pair.base}/${props.pair.quote} liquidity`}
+              xAxisData={graphData.map((_) => _.blockNumber)}
+              xAxisLabel="Block"
+              leftYAxis={{ min: 0, formatter: FriendlyFormatNumber }}
+              rightYAxis={{ min: 0, formatter: FriendlyFormatNumber }}
+              leftAxisSeries={[
+                {
+                  label: `Amount ${props.pair.base} sold`,
+                  data: graphData.map((_) => _.blockNumber),
+                  formatter: FriendlyFormatNumber
+                }
+              ]}
+              rightAxisSeries={[
+                {
+                  label: `Amount ${props.pair.quote} received`,
+                  data: graphData.map((_) => _.blockNumber),
+                  formatter: FriendlyFormatNumber
+                }
+              ]}
+            />
+          </Grid>
+        </Grid>
+      )}
 
-            <SimpleAlert alertMsg={alertMsg} handleCloseAlert={handleCloseAlert} openAlert={openAlert} />
-        </>
-    );
+      <SimpleAlert alertMsg={alertMsg} handleCloseAlert={handleCloseAlert} openAlert={openAlert} />
+    </>
+  );
 }
--- src/routes/risklevels/RiskLevels.tsx
+++ src/routes/risklevels/RiskLevels.tsx
@@ -1,115 +1,116 @@
-import { Box, Grid, InputAdornment, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import DataService from "../../services/DataService";
-import { Pair } from "../../models/ApiData";
-import { sleep } from "../../utils/Utils";
-import { SimpleAlert } from "../../components/SimpleAlert";
-import { RiskLevelGraphs } from "./RiskLevelGraph";
+import { Box, Grid, InputAdornment, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import DataService from '../../services/DataService';
+import { Pair } from '../../models/ApiData';
+import { sleep } from '../../utils/Utils';
+import { SimpleAlert } from '../../components/SimpleAlert';
+import { RiskLevelGraphs } from './RiskLevelGraph';
 
 export default function RiskLevels() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [availablePairs, setAvailablePairs] = useState<Pair[]>([]);
-    const [selectedPair, setSelectedPair] = useState<Pair>();
-    const [openAlert, setOpenAlert] = useState(false);
-    const [alertMsg, setAlertMsg] = useState('');
-    const [supplyCap, setSupplyCap] = useState(100);
+  const [isLoading, setIsLoading] = useState(true);
+  const [availablePairs, setAvailablePairs] = useState<Pair[]>([]);
+  const [selectedPair, setSelectedPair] = useState<Pair>();
+  const [openAlert, setOpenAlert] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
+  const [supplyCap, setSupplyCap] = useState(100);
 
-    const handleCloseAlert = () => {
-        setOpenAlert(false);
-    };
-    const handleChangePair = (event: SelectChangeEvent) => {
-        console.log(`handleChangePair: ${event.target.value}`);
-        setSelectedPair({ base: event.target.value.split('/')[0], quote: event.target.value.split('/')[1] });
-    };
-    const handleChangeSupplyCap = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target && event.target.value) {
-            setSupplyCap(Number(event.target.value));
-        }
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
+  const handleChangePair = (event: SelectChangeEvent) => {
+    console.log(`handleChangePair: ${event.target.value}`);
+    setSelectedPair({ base: event.target.value.split('/')[0], quote: event.target.value.split('/')[1] });
+  };
+  const handleChangeSupplyCap = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target && event.target.value) {
+      setSupplyCap(Number(event.target.value));
     }
+  };
 
-    //// useEffect to load data
-    useEffect(() => {
-        setIsLoading(true);
-        // Define an asynchronous function
-        async function fetchData() {
-            try {
-                const data = await DataService.GetAvailablePairs('all');
-                setAvailablePairs(data);
+  //// useEffect to load data
+  useEffect(() => {
+    setIsLoading(true);
+    // Define an asynchronous function
+    async function fetchData() {
+      try {
+        const data = await DataService.GetAvailablePairs('all');
+        setAvailablePairs(data);
 
-                const oldPair = selectedPair;
+        const oldPair = selectedPair;
 
-                if (oldPair && data.some((_) => _.base == oldPair.base && _.quote == oldPair.quote)) {
-                    setSelectedPair(oldPair);
-                } else {
-                    setSelectedPair(data[0]);
-                }
-                await sleep(1); // without this sleep, update the graph before changing the selected pair. so let it here
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                setOpenAlert(true);
-                setIsLoading(false);
-                if (error instanceof Error) {
-                    setAlertMsg(`Error fetching data: ${error.toString()}`);
-                } else {
-                    setAlertMsg(`Unknown error`);
-                }
-            }
+        if (oldPair && data.some((_) => _.base == oldPair.base && _.quote == oldPair.quote)) {
+          setSelectedPair(oldPair);
+        } else {
+          setSelectedPair(data[0]);
         }
-        fetchData()
-            .then(() => setIsLoading(false))
-            .catch(console.error);
-    }, []);
-
-    if (!selectedPair) {
-        return <Box>BITCONNNEEEEEEEEEEECT</Box>;
+        await sleep(1); // without this sleep, update the graph before changing the selected pair. so let it here
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAlert(true);
+        setIsLoading(false);
+        if (error instanceof Error) {
+          setAlertMsg(`Error fetching data: ${error.toString()}`);
+        } else {
+          setAlertMsg(`Unknown error`);
+        }
+      }
     }
-    return (<Box sx={{ mt: 10 }}>
-        {isLoading ? (
-            <Box>BITCONNNEEEEEEEEEEECT</Box>
-        ) : (
-            <Grid container spacing={1} alignItems="baseline">
-                {/* First row: pairs select and slippage select */}
-                <Grid item xs={6} sm={3}>
-                    <Typography textAlign={'right'}>Pair: </Typography>
-                </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Select
-                        labelId="pair-select"
-                        id="pair-select"
-                        value={`${selectedPair.base}/${selectedPair.quote}`}
-                        label="Pair"
-                        onChange={handleChangePair}
-                    >
-                        {availablePairs.map((pair, index) => (
-                            <MenuItem key={index} value={`${pair.base}/${pair.quote}`}>
-                                {`${pair.base}/${pair.quote}`}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </Grid>
+    fetchData()
+      .then(() => setIsLoading(false))
+      .catch(console.error);
+  }, []);
 
-                <Grid item xs={6} sm={3}>
-                    <Typography textAlign={'right'}>Supply Cap: </Typography>
+  if (!selectedPair) {
+    return <Box>BITCONNNEEEEEEEEEEECT</Box>;
+  }
+  return (
+    <Box sx={{ mt: 10 }}>
+      {isLoading ? (
+        <Box>BITCONNNEEEEEEEEEEECT</Box>
+      ) : (
+        <Grid container spacing={1} alignItems="baseline">
+          {/* First row: pairs select and slippage select */}
+          <Grid item xs={6} sm={3}>
+            <Typography textAlign={'right'}>Pair: </Typography>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Select
+              labelId="pair-select"
+              id="pair-select"
+              value={`${selectedPair.base}/${selectedPair.quote}`}
+              label="Pair"
+              onChange={handleChangePair}
+            >
+              {availablePairs.map((pair, index) => (
+                <MenuItem key={index} value={`${pair.base}/${pair.quote}`}>
+                  {`${pair.base}/${pair.quote}`}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
 
-                </Grid>
-                <Grid item xs={6} sm={3}>
-                    <TextField
-                        required
-                        id="supply-cap-input"
-                        type="number"
-                        label="In Kind"
-                        value={supplyCap}
-                        onChange={handleChangeSupplyCap}
-                        InputProps={{
-                            // startAdornment: <InputAdornment position='start'>$</InputAdornment>,
-                            endAdornment: <InputAdornment position='end'>{selectedPair.base}</InputAdornment>
-                        }}
-                    />
-                </Grid>
+          <Grid item xs={6} sm={3}>
+            <Typography textAlign={'right'}>Supply Cap: </Typography>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              required
+              id="supply-cap-input"
+              type="number"
+              label="In Kind"
+              value={supplyCap}
+              onChange={handleChangeSupplyCap}
+              InputProps={{
+                // startAdornment: <InputAdornment position='start'>$</InputAdornment>,
+                endAdornment: <InputAdornment position="end">{selectedPair.base}</InputAdornment>
+              }}
+            />
+          </Grid>
           <RiskLevelGraphs pair={selectedPair} supplyCap={supplyCap} platform={'all'} />
-            </Grid>
-        )}
+        </Grid>
+      )}
 
-        <SimpleAlert alertMsg={alertMsg} handleCloseAlert={handleCloseAlert} openAlert={openAlert} />
-    </Box>)
+      <SimpleAlert alertMsg={alertMsg} handleCloseAlert={handleCloseAlert} openAlert={openAlert} />
+    </Box>
+  );
 }
