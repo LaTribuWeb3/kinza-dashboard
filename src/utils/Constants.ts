82dbe477e1bc21858fe043c24923499ebc40cc1d
Alfu <Alfulinku@gmail.com>
2024-02-01 12:37:38 +0100

    fix map

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -2,8 +2,8 @@ export const DATA_SOURCES = ['All sources', 'Pancakeswap v2', 'Pancakeswap v3'];
 
 export const DATA_SOURCES_MAP = {
   'All sources': 'all',
-  'Uniswap v2': 'pancakeswapv2',
-  'Uniswap v3': 'pancakeswapv3',
+  'Pancakeswap v2': 'pancakeswapv2',
+  'Pancakeswap v3': 'pancakeswapv3',
 };
 
 export const SLIPPAGES_BPS = Array.from({ length: 20 }).map((_v, i) => (i + 1) * 100);
