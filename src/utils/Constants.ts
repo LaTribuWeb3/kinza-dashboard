01edcb9facbc793cf5c675e81a18794f44570556
0xVorian <33686588+0xVorian@users.noreply.github.com>
2024-02-09 17:45:38 +0100

    updated source map to include wombat

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,9 +1,10 @@
-export const DATA_SOURCES = ['All sources', 'Pancakeswap v2', 'Pancakeswap v3'];
+export const DATA_SOURCES = ['All sources', 'Pancakeswap v2', 'Pancakeswap v3', 'Wombat'];
 
 export const DATA_SOURCES_MAP = {
   'All sources': 'all',
   'Pancakeswap v2': 'pancakeswapv2',
   'Pancakeswap v3': 'pancakeswapv3',
+  'Wombat': 'wombat'
 };
 
 export const SLIPPAGES_BPS = Array.from({ length: 20 }).map((_v, i) => (i + 1) * 100);
